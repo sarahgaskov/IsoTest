c1ef0b1a2ad2d475f841bf35ad489d9a9318b587
sarahgaskov <seva.gaskov@gmail.com>
2026-07-30 14:15:42 -0700

    Lighting cont

--- scripts/dungeon/levels/level.gd
+++ scripts/dungeon/levels/level.gd
@@ -1,8 +1,16 @@
+@tool
 extends Node3D
 class_name Level
+
+## Rebake the level's lighting from every tile. Editor only - it writes to disk.
+@export_tool_button("Bake lighting") var _bake = rebake_lighting
 
 @onready var lighting = $Lighting
 @onready var tiles =    $Layers/Floor
 
 func _ready():
-	lighting.bake(tiles)
+	if Engine.is_editor_hint(): return
+	lighting.bake(self)
+
+func rebake_lighting() -> void:
+	$Lighting.rebake(self)
